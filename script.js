162e8407dfdcab05268942ca0365a78a80a2a4fb
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2023-02-07 22:16:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+//define the variables
+let sources = "bbc-news";
+let apiKey = "6de42d35ceb8407591043c0aa1699455";
+let newsText = document.getElementById('news');
+let hotKey='top-headlines'
+
+
+
+
+// Create a get request
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `https://newsapi.org/v2/${hotKey}?sources=${sources}&apiKey=${apiKey}`, true);
+xhr.getResponseHeader('Content-type', 'application/json');
+
+// What to do when response is ready
+xhr.onload = function () {
+    if (this.status === 200) {
+
+        let jsonText = JSON.parse(this.responseText);
+        let articles = jsonText.articles;
+        let newsHtml="";
+        // console.log(articles);
+        articles.forEach(news => {
+            
+         
+           
+            let newsBody = `
+            <div class="col ">
+            <div class="card bg-info border-3 border-dark mb-2 h-100" >
+                            <img src=${news.urlToImage} class="card-img-top" alt="Images">
+                            <div class="card-body">
+                            <h5 class="card-title">${news.title}</h5>
+                            <p class="card-text" >${news.content}</p>
+                            <a href=${news.url} target="_blank" class="btn btn-dark mt-auto">See full News</a>
+                            </div>
+                        </div>
+                        </div>`;
+            newsHtml+=newsBody;
+
+        });
+        document.getElementById('news').innerHTML=newsHtml;
+    }
+    else {
+        console.log("Some error occured");
+    }
+}
+
+xhr.send()
+
